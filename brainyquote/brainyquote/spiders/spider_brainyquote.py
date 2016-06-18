80d696e2794fba6944f9b2508be5204311498584
ITEXICO <itexico@MacBook-Pro-de-Admin.local>
2016-06-18 16:19:33 -0500

    img parseada

--- brainyquote/brainyquote/spiders/spider_brainyquote.py
+++ brainyquote/brainyquote/spiders/spider_brainyquote.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import scrapy ,urlparse
+import scrapy, urlparse
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from brainyquote.items import BrainyquoteItem
@@ -21,29 +21,31 @@ class SpiderBrainyquoteSpider(CrawlSpider):
 
     def first_quotes(self, response):        
         for individual_author in response.xpath('//div[contains(@class, "bq_s")]//div[contains(@class, "bq_fl")]//div[contains(@class, "bqLn")]'):
-            author_name = individual_author.xpath('./a/text()').extract_first()
-            author_link = individual_author.xpath('./a/@href').extract_first()
-            full_author_link = urlparse.urljoin(response.url,author_link)
+            author_name      = individual_author.xpath('./a/text()').extract_first()
+            author_link      = individual_author.xpath('./a/@href').extract_first()
+            full_author_link = urlparse.urljoin(response.url, author_link)
 
-            item = Brainyquote(item)
+            item                = BrainyquoteItem()
             item['author_name'] = author_name
-            yield scrapy.Request(url=full_author_link, callback=self.parse_autor, meta={'item':item})
-            self.logger.info('INFORMATION: {0} - {1}'.format(author_name, full_author_link))
-            break            
+
+            yield scrapy.Request(url=full_author_link, callback=self.parse_author, meta={'item': item})
+            break
         ## End for loop individual author
     ## End first_quotes
 
     def parse_author(self, response):
         item = BrainyquoteItem(response.request.meta['item'])
-        print item
+        
+        for i, individual_quote in enumerate(response.xpath('//div[contains(@id, "quotesList")]/div')):
+            img_path = individual_quote.xpath('./a/img/@src').extract_first()
+            full_img_path = None
+            if img_path:
+                full_img_path = urlparse.urljoin(response.url, img_path)
+                
+            self.logger.info('Quote: {} - {}'.format(i, img_path))
+            if i > 2:
+                break
+        ## End for loop i
 
-        yeld None
-
-    ## End of parse_author
-
-    def parse_item(self, response):        
-        i = BrainyquoteItem()
-        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
-        #i['name'] = response.xpath('//div[@id="name"]').extract()
-        #i['description'] = response.xpath('//div[@id="description"]').extract()
-        return i
+        yield None
+    ## End parse_author
