933a1dd27dbb39ea2759d3d2eb553eeb62cfabea
ITEXICO <itexico@MacBook-Pro-de-Admin.local>
2016-06-18 15:42:23 -0500

    elbuenoorasi

--- brainyquote/brainyquote/spiders/spider_brainyquote.py
+++ brainyquote/brainyquote/spiders/spider_brainyquote.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import logging
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from brainyquote.items import BrainyquoteItem
@@ -21,12 +20,12 @@ class SpiderBrainyquoteSpider(CrawlSpider):
     ## End start_requests
 
     def first_quotes(self, response):        
-        inspect_response(response, self)
-        for individual_author in response.xpath('//div[contains(@class, "bq_s")]//div[contains(@class, "bq_fl")]//div[contains(@class ,"bqLn")]'):
+        for individual_author in response.xpath('//div[contains(@class, "bq_s")]//div[contains(@class, "bq_fl")]//div[contains(@class, "bqLn")]'):
             author_name = individual_author.xpath('./a/text()').extract_first()
             author_link = individual_author.xpath('./a/@href').extract_first()
             self.logger.info('INFORMATION: {0} - {1}'.format(author_name, author_link))
-
+            # break            
+        ## End for loop individual author
     ## End first_quotes
 
     def parse_item(self, response):        
