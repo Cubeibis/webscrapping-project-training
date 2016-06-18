02dabc8f7f5e4ca7bbac1a9f5e51b8b2f1ea1889
ITEXICO <itexico@MacBook-Pro-de-Admin.local>
2016-06-18 13:14:46 -0500

    xxx

--- brainyquote/brainyquote/spiders/spider_brainyquote.py
+++ brainyquote/brainyquote/spiders/spider_brainyquote.py
@@ -2,31 +2,28 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from brainyquote.items import BrainyquoteItem
 from scrapy.shell import inspect_response
 
-from brainyquote.items import BrainyquoteItem
-
-
 class SpiderBrainyquoteSpider(CrawlSpider):
-    name = 'spider_brainyquote'
+    name            = 'spider_brainyquote'
     allowed_domains = ['brainyquote.com']
-    start_url = ['http://www.brainyquote.com/']
-    #start_urls = ['http://www.brainyquote.com/']
+    start_url       = 'http://www.brainyquote.com'
+    # start_urls      = ['http://www.brainyquote.com']
 
     rules = (
         Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
     )
 
-    def start_request(self):
-        return[scrapy.Request(url=self.start_url,callback=self.first_quotes)]
-    ## End of start requests
+    def start_requests(self):
+        return [scrapy.Request(url=self.start_url, callback=self.first_quotes)]
+    ## End start_requests
 
-    def first_quotes(self,response):
+    def first_quotes(self, response):        
         inspect_response(response, self)
+    ## End first_quotes
 
-    ## End of first quotes
-
-    def parse_item(self, response):
+    def parse_item(self, response):        
         i = BrainyquoteItem()
         #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
         #i['name'] = response.xpath('//div[@id="name"]').extract()
