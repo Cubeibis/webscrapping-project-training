9275f7dcb2ffb43f3c8325fd130110ae49fad753
ITEXICO <itexico@MacBook-Pro-de-Admin.local>
2016-06-18 13:01:12 -0500

    Scrappy test

--- brainyquote/brainyquote/spiders/spider_brainyquote.py
+++ brainyquote/brainyquote/spiders/spider_brainyquote.py
@@ -2,6 +2,7 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from scrapy.shell import inspect_response
 
 from brainyquote.items import BrainyquoteItem
 
@@ -9,11 +10,21 @@ from brainyquote.items import BrainyquoteItem
 class SpiderBrainyquoteSpider(CrawlSpider):
     name = 'spider_brainyquote'
     allowed_domains = ['brainyquote.com']
-    start_urls = ['http://www.brainyquote.com/']
+    start_url = ['http://www.brainyquote.com/']
+    #start_urls = ['http://www.brainyquote.com/']
 
     rules = (
         Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
     )
+
+    def start_request(self):
+        return[scrapy.Request(url=self.start_url,callback=self.first_quotes)]
+    ## End of start requests
+
+    def first_quotes(self,response):
+        inspect_response(response, self)
+
+    ## End of first quotes
 
     def parse_item(self, response):
         i = BrainyquoteItem()
