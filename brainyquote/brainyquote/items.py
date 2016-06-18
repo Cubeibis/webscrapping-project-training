16867dd9c7e5baf72c2a907c3e4a4d5b9cae428e
ITEXICO <itexico@MacBook-Pro-de-Admin.local>
2016-06-18 16:04:12 -0500

    we need to go deeper

--- brainyquote/brainyquote/items.py
+++ brainyquote/brainyquote/items.py
@@ -11,4 +11,5 @@ import scrapy
 class BrainyquoteItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    author_name = scrapy.Field()
+    quote = scrapy.Field()
--- brainyquote/brainyquote/spiders/spider_brainyquote.py
+++ brainyquote/brainyquote/spiders/spider_brainyquote.py
@@ -24,10 +24,22 @@ class SpiderBrainyquoteSpider(CrawlSpider):
             author_name = individual_author.xpath('./a/text()').extract_first()
             author_link = individual_author.xpath('./a/@href').extract_first()
             full_author_link = urlparse.urljoin(response.url,author_link)
+
+            item = Brainyquote(item)
+            item['author_name'] = author_name
+            yield scrapy.Request(url=full_author_link, callback=self.parse_autor, meta={'item':item})
             self.logger.info('INFORMATION: {0} - {1}'.format(author_name, full_author_link))
-            # break            
+            break            
         ## End for loop individual author
     ## End first_quotes
+
+    def parse_author(self, response):
+        item = BrainyquoteItem(response.request.meta['item'])
+        print item
+
+        yeld None
+
+    ## End of parse_author
 
     def parse_item(self, response):        
         i = BrainyquoteItem()
